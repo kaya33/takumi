f9b906f88ba4d5c06dcfb1fc0449359d6d85adf8
maralla <maralla.ai@gmail.com>
2017-05-04 15:17:51 +0800

    Bump version to v0.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='takumi',
-    version='0.1.3',
+    version='0.1.4',
     description='Takumi service framework',
     long_description=open("README.rst").read(),
     author="Eleme Lab",
